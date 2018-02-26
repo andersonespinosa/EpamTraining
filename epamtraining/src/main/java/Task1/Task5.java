0bad67ccf2354673913634237fb18566334e9ef2
Ivan Prodan <ivan.d.prodan@gmail.com>
2018-02-27 01:46:21 +0300

    Added Task5

---
+++ epamtraining/src/main/java/Task1/Task5.java
@@ -0,0 +1,26 @@
+package Task1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Task5 matrix = new Task5();
+        int side = 8;
+        matrix.printArray(side);
+    }
+
+    public void printArray(int side) {
+        int[][] a = new int[side][side];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = i == j || i == a.length - j - 1 ? 1 : 0;
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
