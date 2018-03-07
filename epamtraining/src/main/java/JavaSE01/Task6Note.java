64ecd5b936c8a8c6b4b24f21cb3b2d888bc935c5
cors4c <33065055+cors4c@users.noreply.github.com>
2018-03-07 11:12:34 +0300

    Added javadoc comments

---
+++ epamtraining/src/main/java/JavaSE01/Task6Note.java
@@ -0,0 +1,15 @@
+package JavaSE01;
+
+public class Task6Note {
+    private String note;
+
+    public String getNote() {
+
+        return note;
+    }
+
+    public void setNote(String note) {
+
+        this.note = note;
+    }
+}
