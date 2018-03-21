0d30aa5820334d90cab8ad6fec5bff4714bec61a
ivan-prodan <33065055+cors4c@users.noreply.github.com>
2018-03-21 17:59:20 +0300

    Added Task5

--- epamtraining/src/main/java/JavaSE02/Task5/Student.java
+++ epamtraining/src/main/java/JavaSE02/Task5/Student.java
@@ -1,10 +1,22 @@
 package JavaSE02.Task5;
 
-public class Student {
+import java.util.EnumMap;
+
+public class Student<T extends Number> {
     private String name;
     private int id;
+    private EnumMap<Subjects, T> subjectsToMarks;
+
+    public void getMarksBySubject(Subjects subject) {
+        subjectsToMarks.get(subject);
+    }
+
+    public void setMarkBySubject(Subjects subject, T mark) {
+        subjectsToMarks.put(subject, mark);
+    }
 
     public String getName() {
+
         return name;
     }
 
@@ -13,10 +25,12 @@ public class Student {
     }
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 }
--- epamtraining/src/main/java/JavaSE02/Task5/StudyGroup.java
+++ epamtraining/src/main/java/JavaSE02/Task5/StudyGroup.java
@@ -1,15 +1,31 @@
 package JavaSE02.Task5;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
-public class StudyGroup {
+public class StudyGroup<T extends Number> {
+    private Subjects subject;
+
     private static final ArrayList<Student> students = new ArrayList<>();
 
-    public void addStudentToGroup(Student student){
+    public StudyGroup(Subjects subject) {
+        this.subject = subject;
+    }
+
+    public void addStudentToGroup(Student student) {
         students.add(student);
     }
 
-    public void getStudentById(int id){
+    public void removeStudentFromGroup(Student student) {
+        students.remove(student);
+    }
 
+    public Optional<Student> getStudentById(int id){
+        for(Student x:students){
+            if(id == x.getId()){
+                return Optional.ofNullable(x);
+            }
+        }
+        return Optional.empty();
     }
 }
