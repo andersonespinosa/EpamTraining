5a496f1bd3433e8e08853c27592eabb228161420
ivan-prodan <33065055+cors4c@users.noreply.github.com>
2018-03-27 16:39:21 +0300

    Add JavaSE03.Task1

---
+++ epamtraining/src/main/java/JavaSE03/Task1/CrazyLogger.java
@@ -0,0 +1,34 @@
+package JavaSE03.Task1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class CrazyLogger {
+    private StringBuilder logger = new StringBuilder();
+    private LocalDateTime localDateTimeTime = LocalDateTime.now();
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-mm-YYYY : hh-mm - ");
+
+    private void log(String str) {
+        logger.append(localDateTimeTime.format(dateTimeFormatter) + str + System.getProperty("line.separator"));
+    }
+
+    private void printLog() {
+        try (FileOutputStream outputStream = new FileOutputStream("D:\\Java/log.txt");
+             OutputStreamWriter writer = new OutputStreamWriter(outputStream)) {
+            writer.write(logger.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        CrazyLogger crazyLogger = new CrazyLogger();
+        crazyLogger.log("Log this info");
+        crazyLogger.log("Log this info too");
+        crazyLogger.printLog();
+    }
+}
