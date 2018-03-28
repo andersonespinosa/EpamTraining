ecaf5f9772961be9dee0193780e0b261b6b84982
ivan-prodan <33065055+cors4c@users.noreply.github.com>
2018-03-28 15:29:30 +0300

    Add search by date in JavaSe03.Task1.CrazyLogger

--- epamtraining/src/main/java/JavaSE03/Task1/CrazyLogger.java
+++ epamtraining/src/main/java/JavaSE03/Task1/CrazyLogger.java
@@ -1,23 +1,22 @@
 package JavaSE03.Task1;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class CrazyLogger {
     private StringBuilder logger = new StringBuilder();
     private LocalDateTime localDateTimeTime = LocalDateTime.now();
-    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-mm-YYYY : hh-mm - ");
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMM-YYYY : hh-mm - ", Locale.ENGLISH);
 
     private void log(String str) {
         logger.append(localDateTimeTime.format(dateTimeFormatter)).append(str).append(System.getProperty("line.separator"));
     }
 
     private void printLog() {
-        try (FileOutputStream outputStream = new FileOutputStream("epamtraining/src/main/java/JavaSE03/Task1/log.txt");
+        try (FileOutputStream outputStream = new FileOutputStream("epamtraining/src/main/java/JavaSE03/Task1/log.txt", true);
              OutputStreamWriter writer = new OutputStreamWriter(outputStream)) {
             writer.write(logger.toString());
         } catch (IOException e) {
@@ -25,10 +24,32 @@ public class CrazyLogger {
         }
     }
 
-    public static void main(String[] args) {
-        CrazyLogger crazyLogger = new CrazyLogger();
-        crazyLogger.log("Log this info");
-        crazyLogger.log("Log this info too");
-        crazyLogger.printLog();
+    private String findInfoInLog(String searchedString) {
+        try (Scanner scanner = new Scanner(new File("epamtraining/src/main/java/JavaSE03/Task1/log.txt"))) {
+            while (scanner.hasNextLine()) {
+                String logString = scanner.nextLine();
+                if (logString.toLowerCase().contains(searchedString.toLowerCase())) {
+                    return logString;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "No such info in log file";
+    }
+
+    private String findInfoInLog(LocalDateTime dateTime) {
+        try (Scanner scanner = new Scanner(new File("epamtraining/src/main/java/JavaSE03/Task1/log.txt"))) {
+            String formattedDate = dateTime.format(dateTimeFormatter);
+            while (scanner.hasNextLine()) {
+                String logString = scanner.nextLine();
+                if (logString.toLowerCase().contains(formattedDate.toLowerCase())) {
+                    return logString;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "No such date in log file";
     }
 }
