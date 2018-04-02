fc508044f5b72c60feac6bda57aca98d2eee6429
Ivan Prodan <ivan.d.prodan@gmail.com>
2018-04-02 22:02:01 +0300

    Having some issues

--- epamtraining/src/main/java/JavaSE04/Task1/KeywordParser.java
+++ epamtraining/src/main/java/JavaSE04/Task1/KeywordParser.java
@@ -1,53 +1,45 @@
-package JavaSE04.Task1;
-
-import com.sun.org.apache.xerces.internal.xs.StringList;
+package javase04.Task1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class KeywordParser {
-//    private Path codeFile = Paths.get("D:\\Java\\JavaCode.txt");
-//    private Path keywordsFile = Paths.get("D:\\Java\\JavaKeywords.txt");
-//    private List<String> codeFileWords = new ArrayList<>();
-//    private List<String> keywordsFileWords = new ArrayList<>();
-//    private List<String> foundKeywords = new ArrayList<>();
 
-    public List<String> parseFile(String filePath) {
-        List<String> codeFileWords = new ArrayList<>();
-        Path codeFilePath = Paths.get(filePath);
-        try (InputStream in = Files.newInputStream(codeFilePath);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                codeFileWords.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return codeFileWords;
-    }
-
-    public List<String> searchForKeywords(List<String> parsedFileStrings) {
-        List<String> keywordsFileWords = new ArrayList<>();//add file JavaKeywords
+    public List<String> parse(String codeFilePath, String keywordsFilePath) {
+        List<String> codeFile = readFromFile(codeFilePath);
+        List<String> keywordsFile = readFromFile(keywordsFilePath);
         List<String> foundKeywords = new ArrayList<>();
 
-        for (String codeFileWord : parsedFileStrings) {
-            for (String keywordsFileWord : keywordsFileWords) {
+        for (String codeFileWord : codeFile) {
+            for (String keywordsFileWord : keywordsFile) {
                 if (codeFileWord.equals(keywordsFileWord)) {
                     foundKeywords.add(keywordsFileWord);
                 }
             }
         }
         return foundKeywords;
+    }
+
+    private List<String> readFromFile(String filePath) {
+        List<String> fileContent = new ArrayList<>();
+        Path path = Paths.get(filePath);
+        try (InputStream in = Files.newInputStream(path);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileContent.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent;
     }
 
 
