27f2113d08cb744ab7f4dacd45164e02733ebec8
ivan-prodan <33065055+cors4c@users.noreply.github.com>
2018-04-03 13:55:37 +0300

    Add javase06.task2

---
+++ epamtraining/src/main/java/javase06/task2/PropertyReader.java
@@ -0,0 +1,27 @@
+package javase06.task2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class PropertyReader {
+    private Map<String, String> propertyMap = new HashMap<>();
+
+    public Map getPropertiesMap(){
+        return propertyMap;
+    }
+
+    public void readPropertyFile(String path, String key){
+        try (InputStream inputStream = new FileInputStream(path)) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            String property = properties.getProperty(key);
+            propertyMap.put(property, "");
+        } catch (IOException e) {
+            throw new RuntimeException("File or property could not be found", e);
+        }
+    }
+}
